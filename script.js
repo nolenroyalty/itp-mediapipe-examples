b22d91a65015119f798b19e854bfc6ca443a6038
Glitch (glitch-hello-website) <none>
2024-06-07 18:29:42 +0000

    🚆☂️ Checkpoint ./script.mjs:68410940/1833 ./index.html:68410940/2299 ./script.js:68410940/6948 ./style.css:68410940/5778

---
+++ script.js
@@ -0,0 +1,71 @@
+const webcamVideo = document.querySelector("#webcamVideo");
+const enableWebcamButton = document.querySelector("#enableWebcamButton");
+
+async function createFaceLandmarker({
+  numFaces,
+  FaceLandmarker,
+  FilesetResolver,
+}) {
+  const filesetResolver = await FilesetResolver.forVisionTasks(
+    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
+  );
+
+  async function getLandmarker() {
+    return FaceLandmarker.createFromOptions(filesetResolver, {
+      baseOptions: {
+        modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+        delegate: "GPU",
+      },
+      outputFaceBlendshapes: true,
+      runningMode: "VIDEO",
+      numFaces: numFaces,
+      minFaceDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.4,
+    });
+  }
+  return getLandmarker();
+}
+
+function enableCam() {
+  console.log("hi");
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (stream) {
+      webcamVideo.srcObject = stream;
+      console.log("enabled");
+    })
+    .catch((err) => {
+      console.error(err);
+      throw new Error(err);
+    });
+}
+
+function wireUpWebcamButton() {
+  const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
+  if (hasGetUserMedia()) {
+    enableWebcamButton.addEventListener("click", enableCam);
+  } else {
+    alert("Couldn't find your webcam. You need a webcam to use this site.");
+  }
+}
+
+import(
+  "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/vision_bundle.js"
+).then((module) => {
+  main({ ...module });
+});
+
+wireUpWebcamButton();
+
+async function main({ FaceLandmarker, FilesetResolver }) {
+  
+  console.log("hi");
+  console.log(FaceLandmarker);
+
+  const landmarker = await createFaceLandmarker({
+    numFaces: 1,
+    FaceLandmarker,
+    FilesetResolver,
+  });
+  console.log(`landmarker: ${landmarker}`);
+}
